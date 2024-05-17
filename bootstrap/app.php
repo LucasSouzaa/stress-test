26c6d0ff181818e32d6928a473d55b9c41a718a9
Lucas Silva <lucas.roberto@creditas.com>
2024-05-17 17:24:01 -0300

    log duration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \ChaseConey\LaravelDatadogHelper\Middleware\LaravelDatadogMiddleware::class
         ]);
 
         $middleware->alias([
