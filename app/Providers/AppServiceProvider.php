bc1eddd4f9c1fceb5be176bd63a9fb332c3b77cd
Lucas Silva <lucas.roberto@creditas.com>
2024-05-17 17:05:29 -0300

    add monolog apm tracer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,37 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $monolog = logger()->getLogger();
+        if (!$monolog instanceof \Monolog\Logger) {
+            return;
+        }
+
+        // Optional: Use JSON formatting
+        $useJson = false;
+        foreach ($monolog->getHandlers() as $handler) {
+            if (method_exists($handler, 'setFormatter')) {
+                $handler->setFormatter(new \Monolog\Formatter\JsonFormatter());
+                $useJson = true;
+            }
+        }
+
+        // Inject the trace and span ID to connect the log entry with the APM trace
+        $monolog->pushProcessor(function ($record) use ($useJson) {
+            $context = \DDTrace\current_context();
+            if ($useJson === true) {
+                $record['extra']['dd'] = [
+                    'trace_id' => $context['trace_id'],
+                    'span_id'  => $context['span_id'],
+                ];
+            } else {
+                $record['message'] .= sprintf(
+                    ' [dd.trace_id=%d dd.span_id=%d]',
+                    $context['trace_id'],
+                    $context['span_id']
+                );
+            }
+            return $record;
+        });
     }
 
     /**
